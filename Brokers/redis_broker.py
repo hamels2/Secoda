09ba97e3bf83bb1cb85e64b5fa26c1a3ff55c84f
Sam <sam2hamel@gmail.com>
2024-01-29 19:26:42 -0500

    Implement logging + dql for Redis Broker

--- Brokers/redis_broker.py
+++ Brokers/redis_broker.py
@@ -3,30 +3,42 @@ from redis import Redis
 
 class RedisBroker(Broker):
 
-    def __init__(self, qid, timeout=1):
+    def __init__(self, queue_id, timeout=1):
         super().__init__()
         self.redis = Redis()
-        self.key = qid
-        self.timeout = max(timeout,1)
-        self.logging_key = qid + '_logging_queue'
-        self.dlq_key = qid + '_DLQ'
+        self.timeout = max(timeout,1) # min is 1 because broker does not support indefinite waits
+        
+        # Broker only supports one queue + corresponding logging queue to log all transactions and dlq
+        self.key = queue_id
+        self.logging_key = queue_id + '_logging_queue'
+        self.dlq_key = queue_id + '_DLQ'
     
-    def enqueue(self, task, task_id):
-
+    def enqueue(self, task: str | bytes, task_id: str) -> None:
         self.redis.rpush(self.key,task)
         queued_message = f'{task_id} queued successfully'
         self.redis.rpush(self.logging_key, queued_message)
     
-    def dequeue(self):
+    def dequeue(self) -> str | bytes | None:
         task = self.redis.blpop([self.key],self.timeout)
         if task is None:
             return None
         return task[1]
-
         
-    def queue_size(self):
+    def queue_size(self) -> int:
         return self.redis.llen(self.key)
 
-    def flush_queue(self):
-        return self.redis.ltrim(self.key,1,0)
+    def flush_queue(self) -> None:
+        self.redis.ltrim(self.key,1,0)
     
+    def log_success(self, task_id: str) -> None:
+        success_message = f'{task_id} processed successfully'
+        self.redis.rpush(self.logging_key, success_message)
+    
+    
+    def log_failure(self, task_id: str) -> None:
+        fail_message = f'{task_id} failed to process'
+        self.redis.rpush(self.logging_key, fail_message)
+    
+    def process_failure(self, task: str | bytes, task_id: str) -> None:
+        self.log_failure(task_id)
+        self.redis.lpush(self.dlq_key, task)
--- Brokers/test/redis_broker_test.py
+++ Brokers/test/redis_broker_test.py
@@ -10,11 +10,15 @@ def cleanup():
     yield
     redis = Redis()
     redis.delete(QUEUE_NAME)
+    redis.delete(QUEUE_NAME + '_logging_queue')
+    redis.delete(QUEUE_NAME + '_DLQ')
 
 def test_init():
     broker = RedisBroker(QUEUE_NAME,0)
     assert(broker.key == QUEUE_NAME)
+    assert(broker.logging_key == QUEUE_NAME + '_logging_queue')
     assert(broker.timeout == 1)
+    assert(broker.dlq_key == QUEUE_NAME + '_DLQ')
 
 
 def test_enqueue():
@@ -23,10 +27,14 @@ def test_enqueue():
     broker.enqueue('Hello', 'Test1')
     
     assert(redis.llen(broker.key) == 1)
+    assert(redis.llen(broker.logging_key) == 1)
+    assert(redis.llen(broker.dlq_key) == 0)
 
     broker.enqueue('Hello1', 'Test2')
     
     assert(redis.llen(broker.key) == 2)
+    assert(redis.llen(broker.logging_key) == 2)
+    assert(redis.llen(broker.dlq_key) == 0)
 
 
 def test_dequeue():
@@ -54,6 +62,34 @@ def test_flush_queue():
     
     broker.flush_queue()
     assert(broker.queue_size() == 0)
+
+def test_log_success():
+    redis = Redis()
+    broker = RedisBroker(QUEUE_NAME,0)
+    broker.log_success('Test1')
+    assert(redis.llen(broker.logging_key) == 1)
+    msg = redis.blpop(broker.logging_key)[1]
+    assert(msg == b'Test1 processed successfully')
+
+def test_log_failure():
+    redis = Redis()
+    broker = RedisBroker(QUEUE_NAME,0)
+    broker.log_failure('Test1')
+    assert(redis.llen(broker.logging_key) == 1)
+    msg = redis.blpop(broker.logging_key)[1]
+    assert(msg == b'Test1 failed to process')
+
+def test_process_failure():
+    redis = Redis()
+    broker = RedisBroker(QUEUE_NAME,0)
+    broker.process_failure('hello','Test1')
+    assert(redis.llen(broker.logging_key) == 1)
+    msg = redis.blpop(broker.logging_key)[1]
+    assert(msg == b'Test1 failed to process')
+
+    assert(redis.llen(broker.dlq_key) == 1)
+    msg = redis.blpop(broker.dlq_key)[1]
+    assert(msg == b'hello')
 
 
     
